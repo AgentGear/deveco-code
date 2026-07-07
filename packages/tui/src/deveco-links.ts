a8ba6c88eca79074749c43220214ffd39ef66aad
anieo <zhangyuchen45@huawei.com>
2026-07-07 11:05:29 +0800

    fix(tui): redirect /feedback to GitCode issues page
    Signed-off-by: anieo <zhangyuchen45@huawei.com>

--- packages/tui/src/deveco-links.ts
+++ packages/tui/src/deveco-links.ts
@@ -4,5 +4,5 @@ export const DEVECO_README_URL = "https://gitcode.com/openharmony-sig/deveco-cod
 /** DevEco Code issue tracker on GitCode. */
 export const DEVECO_ISSUES_URL = "https://gitcode.com/openharmony-sig/deveco-code/issues"
 
-/** Huawei developer feedback portal for DevEco Code. */
-export const DEVECO_FEEDBACK_URL = "https://developer.huawei.com/consumer/cn/support/feedback/#/"
+/** Feedback and issue tracker for DevEco Code on GitCode. */
+export const DEVECO_FEEDBACK_URL = "https://gitcode.com/openharmony-sig/deveco-code/issues"
